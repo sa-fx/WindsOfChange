4a9083048ad059dd49c929751c5fa2ee652352cf
Stewart Alexander <66742157+sa-fx@users.noreply.github.com>
2024-02-24 14:57:52 +0000

    Code compiles

--- src/power_meter.h
+++ src/power_meter.h
@@ -2,16 +2,45 @@
 #define __POWER_METER_H__
 
 #include <ModbusMaster.h>
-// TODOLater - Implement PowerMeter
+
+// Implemented for use with PZEM-014
+
 class PowerMeter {
  public:
-  PowerMeter(uint8_t device_address = 0x01, int pin_de, int pin_re);
+  PowerMeter(uint8_t device_address, int pin_de, int pin_re);
   ~PowerMeter();
+
+  // TODOLater - Determine if calibration function is required
+  // TODOLater - Determine if reset energy function is required
+
+  /**
+   * @returns Voltage value in Volts between 80V and 260V
+   */
   uint32_t getVoltage();
+
+  /**
+   * @returns Current value in Amps between 0 and 10A
+   */
   uint32_t getCurrent();
+
+  /**
+   * @returns Active power value in Watts
+   */
   uint32_t getPower();
+
+  /**
+   * @returns Active energy value in Watt-hours
+   */
   uint32_t getEnergy();
+
+  /**
+   * @returns Frequency value in Hertz between 45Hz and 65Hz
+   */
   uint32_t getFrequency();
+
+  /**
+   * @returns Power factor value between 0 and 1
+   */
   uint32_t getPowerFactor();
 
   /**
@@ -38,7 +67,8 @@ class PowerMeter {
   const uint8_t kRegisterAlarmStatus = 0x0009;
 
   ModbusMaster node_;
-  uint8_t device_address_ int pin_de_;
+  uint8_t device_address_;
+  int pin_de_;
   int pin_re_;
-}
+};
 #endif
