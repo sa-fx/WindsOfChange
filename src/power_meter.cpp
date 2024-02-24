ac8f9bded13602c2b9b69d06dbc438a3c7da8554
Stewart Alexander <66742157+sa-fx@users.noreply.github.com>
2024-02-24 14:08:55 +0000

    PowerMeter functionality added

--- src/power_meter.cpp
+++ src/power_meter.cpp
@@ -1,3 +1,102 @@
 #include "power_meter.h"
 
-// TODOLater - Implement PowerMeter
+PowerMeter::PowerMeter(uint8_t device_address = 0x01, int pin_de, int pin_re)
+    : device_address_(device_address), pin_de_(pin_de), pin_re_(pin_re) {
+  node.begin(device_address_, Serial);
+  pinMode(pin_re_, OUTPUT);
+  pinMode(pin_de_, OUTPUT);
+  digitalWrite(pin_re_, 0);
+  digitalWrite(pin_de_, 0);
+  node.preTransmission(preTransmission);
+  node.postTransmission(postTransmission);
+  // TODOLater - Determine if this is needed
+  // changeAddress(0XF8, 0x01);
+}
+
+PowerMeter::~PowerMeter() {}
+
+uint32_t PowerMeter::getVoltage() {
+  uint8_t result = node.readInputRegisters(kRegisterVOltage, 1);
+  if (result == node.ku8MBSuccess) {
+    // Divide by 10, as per manual
+    return (node.getResponseBuffer(kRegisterVOltage)) / 10;
+  } else {
+    return 0xFFFFFFFF;
+  }
+}
+
+uint32_t PowerMeter::getCurrent() {
+  uint8_t result = node.readInputRegisters(kRegisterCurrentLo, 2);
+  if (result == node.ku8MBSuccess) {
+    // Combine both halves of value and divide by 1000, as per manual
+    return ((node.getResponseBuffer(kRegisterCurrentHi) << 16) +
+            node.getResponseBuffer(kRegisterCurrentLo)) /
+           1000;
+  } else {
+    return 0xFFFFFFFF;
+  }
+}
+
+uint32_t PowerMeter::getPower() {
+  uint8_t result = node.readInputRegisters(kRegisterPowerLo, 2);
+  if (result == node.ku8MBSuccess) {
+    // Combine both halves of value and divide by 10, as per manual
+    return ((node.getResponseBuffer(kRegisterPowerHi) << 16) +
+            node.getResponseBuffer(kRegisterPowerLo)) /
+           10;
+  } else {
+    return 0xFFFFFFFF;
+  }
+}
+
+uint32_t PowerMeter::getEnergy() {
+  uint8_t result = node.readInputRegisters(kRegisterEnergyLo, 2);
+  if (result == node.ku8MBSuccess) {
+    // Combine both halves of value
+    return (node.getResponseBuffer(kRegisterEnergyHi) << 16) +
+           node.getResponseBuffer(kRegisterEnergyLo);
+  } else {
+    return 0xFFFFFFFF;
+  }
+}
+
+uint32_t PowerMeter::getFrequency() {
+  uint8_t result = node.readInputRegisters(kRegisterFrequency, 1);
+  if (result == node.ku8MBSuccess) {
+    // Divide by 10, as per manual
+    return (node.getResponseBuffer(kRegisterFrequency)) / 10;
+  } else {
+    return 0xFFFFFFFF;
+  }
+}
+
+uint32_t PowerMeter::getPowerFactor() {
+  uint8_t result = node.readInputRegisters(kRegisterPowerFactor, 1);
+  if (result == node.ku8MBSuccess) {
+    // Divide by 10, as per manual
+    return (node.getResponseBuffer(kRegisterPowerFactor)) / 100;
+  } else {
+    return 0xFFFFFFFF;
+  }
+}
+
+uint32_t PowerMeter::getAlarmStatus() {
+  uint8_t result = node.readInputRegisters(kRegisterVOltage, 1);
+  if (result == node.ku8MBSuccess) {
+    return node.getResponseBuffer(kRegisterVOltage);
+  } else {
+    return 0x0001;
+  }
+}
+
+void PowerMeter::preTransmission() {
+  digitalWrite(pin_re, HIGH);
+  digitalWrite(pin_de, HIGH);
+  delay(1);
+}
+
+void PowerMeter::postTransmission() {
+  digitalWrite(pin_re, LOW);
+  digitalWrite(pin_de, LOW);
+  delay(1);
+}
--- src/power_meter.h
+++ src/power_meter.h
@@ -1,6 +1,44 @@
 #ifndef __POWER_METER_H__
 #define __POWER_METER_H__
 
+#include <ModbusMaster.h>
 // TODOLater - Implement PowerMeter
+class PowerMeter {
+ public:
+  PowerMeter(uint8_t device_address = 0x01, int pin_de, int pin_re);
+  ~PowerMeter();
+  uint32_t getVoltage();
+  uint32_t getCurrent();
+  uint32_t getPower();
+  uint32_t getEnergy();
+  uint32_t getFrequency();
+  uint32_t getPowerFactor();
 
+  /**
+   * @returns 0x0000 for no alarm;
+   * 0xFFFF for alarm;
+   * 0x0001 for error
+   */
+  uint32_t getAlarmStatus();
+
+ private:
+  void preTransmission();
+  void postTransmission();
+
+ private:
+  const uint8_t kRegisterVoltage = 0x0000;
+  const uint8_t kRegisterCurrentLo = 0x0001;
+  const uint8_t kRegisterCurrentHi = 0x0002;
+  const uint8_t kRegisterPowerLo = 0x0003;
+  const uint8_t kRegisterPowerHi = 0x0004;
+  const uint8_t kRegisterEnergyLo = 0x0005;
+  const uint8_t kRegisterEnergyHi = 0x0006;
+  const uint8_t kRegisterFrequency = 0x0007;
+  const uint8_t kRegisterPowerFactor = 0x0008;
+  const uint8_t kRegisterAlarmStatus = 0x0009;
+
+  ModbusMaster node_;
+  uint8_t device_address_ int pin_de_;
+  int pin_re_;
+}
 #endif
