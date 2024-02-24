701c885c92ea9aa1fc4945039d083852142bfcde
Stewart Alexander <66742157+sa-fx@users.noreply.github.com>
2024-02-24 14:57:52 +0000

    Code compiles

--- src/power_meter.cpp
+++ src/power_meter.cpp
@@ -1,102 +1,133 @@
 #include "power_meter.h"
 
-PowerMeter::PowerMeter(uint8_t device_address = 0x01, int pin_de, int pin_re)
+// TODOLater - Test with hardware
+PowerMeter::PowerMeter(uint8_t device_address, int pin_de, int pin_re)
     : device_address_(device_address), pin_de_(pin_de), pin_re_(pin_re) {
-  node.begin(device_address_, Serial);
+  node_.begin(device_address_, Serial);
   pinMode(pin_re_, OUTPUT);
   pinMode(pin_de_, OUTPUT);
   digitalWrite(pin_re_, 0);
   digitalWrite(pin_de_, 0);
-  node.preTransmission(preTransmission);
-  node.postTransmission(postTransmission);
+  // TODOLater - Decide if removing this is okay
+  //   node_.preTransmission(preTransmission);
+  //   node_.postTransmission(postTransmission);
   // TODOLater - Determine if this is needed
   // changeAddress(0XF8, 0x01);
 }
 
 PowerMeter::~PowerMeter() {}
 
+// TODOLater - Refactor getters to remove code duplication
 uint32_t PowerMeter::getVoltage() {
-  uint8_t result = node.readInputRegisters(kRegisterVOltage, 1);
-  if (result == node.ku8MBSuccess) {
+  uint32_t measurement;
+  preTransmission();
+  uint8_t result = node_.readInputRegisters(kRegisterVoltage, 1);
+  if (result == node_.ku8MBSuccess) {
     // Divide by 10, as per manual
-    return (node.getResponseBuffer(kRegisterVOltage)) / 10;
+    measurement = (node_.getResponseBuffer(kRegisterVoltage)) / 10;
   } else {
-    return 0xFFFFFFFF;
+    measurement = 0xFFFFFFFF;
   }
+  postTransmission();
+  return measurement;
 }
 
 uint32_t PowerMeter::getCurrent() {
-  uint8_t result = node.readInputRegisters(kRegisterCurrentLo, 2);
-  if (result == node.ku8MBSuccess) {
+  uint32_t measurement;
+  preTransmission();
+  uint8_t result = node_.readInputRegisters(kRegisterCurrentLo, 2);
+  if (result == node_.ku8MBSuccess) {
     // Combine both halves of value and divide by 1000, as per manual
-    return ((node.getResponseBuffer(kRegisterCurrentHi) << 16) +
-            node.getResponseBuffer(kRegisterCurrentLo)) /
-           1000;
+    measurement = ((node_.getResponseBuffer(kRegisterCurrentHi) << 16) +
+                   node_.getResponseBuffer(kRegisterCurrentLo)) /
+                  1000;
   } else {
-    return 0xFFFFFFFF;
+    measurement = 0xFFFFFFFF;
   }
+  postTransmission();
+  return measurement;
 }
 
 uint32_t PowerMeter::getPower() {
-  uint8_t result = node.readInputRegisters(kRegisterPowerLo, 2);
-  if (result == node.ku8MBSuccess) {
+  uint32_t measurement;
+  preTransmission();
+  uint8_t result = node_.readInputRegisters(kRegisterPowerLo, 2);
+  if (result == node_.ku8MBSuccess) {
     // Combine both halves of value and divide by 10, as per manual
-    return ((node.getResponseBuffer(kRegisterPowerHi) << 16) +
-            node.getResponseBuffer(kRegisterPowerLo)) /
-           10;
+    measurement = ((node_.getResponseBuffer(kRegisterPowerHi) << 16) +
+                   node_.getResponseBuffer(kRegisterPowerLo)) /
+                  10;
   } else {
-    return 0xFFFFFFFF;
+    measurement = 0xFFFFFFFF;
   }
+  postTransmission();
+  return measurement;
 }
 
 uint32_t PowerMeter::getEnergy() {
-  uint8_t result = node.readInputRegisters(kRegisterEnergyLo, 2);
-  if (result == node.ku8MBSuccess) {
+  uint32_t measurement;
+  preTransmission();
+  uint8_t result = node_.readInputRegisters(kRegisterEnergyLo, 2);
+  if (result == node_.ku8MBSuccess) {
     // Combine both halves of value
-    return (node.getResponseBuffer(kRegisterEnergyHi) << 16) +
-           node.getResponseBuffer(kRegisterEnergyLo);
+    measurement = (node_.getResponseBuffer(kRegisterEnergyHi) << 16) +
+                  node_.getResponseBuffer(kRegisterEnergyLo);
   } else {
-    return 0xFFFFFFFF;
+    measurement = 0xFFFFFFFF;
   }
+  postTransmission();
+  return measurement;
 }
 
 uint32_t PowerMeter::getFrequency() {
-  uint8_t result = node.readInputRegisters(kRegisterFrequency, 1);
-  if (result == node.ku8MBSuccess) {
+  uint32_t measurement;
+  preTransmission();
+  uint8_t result = node_.readInputRegisters(kRegisterFrequency, 1);
+  if (result == node_.ku8MBSuccess) {
     // Divide by 10, as per manual
-    return (node.getResponseBuffer(kRegisterFrequency)) / 10;
+    measurement = (node_.getResponseBuffer(kRegisterFrequency)) / 10;
   } else {
-    return 0xFFFFFFFF;
+    measurement = 0xFFFFFFFF;
   }
+  postTransmission();
+  return measurement;
 }
 
 uint32_t PowerMeter::getPowerFactor() {
-  uint8_t result = node.readInputRegisters(kRegisterPowerFactor, 1);
-  if (result == node.ku8MBSuccess) {
+  uint32_t measurement;
+  preTransmission();
+  uint8_t result = node_.readInputRegisters(kRegisterPowerFactor, 1);
+  if (result == node_.ku8MBSuccess) {
     // Divide by 10, as per manual
-    return (node.getResponseBuffer(kRegisterPowerFactor)) / 100;
+    measurement = (node_.getResponseBuffer(kRegisterPowerFactor)) / 100;
   } else {
-    return 0xFFFFFFFF;
+    measurement = 0xFFFFFFFF;
   }
+  postTransmission();
+  return measurement;
 }
 
 uint32_t PowerMeter::getAlarmStatus() {
-  uint8_t result = node.readInputRegisters(kRegisterVOltage, 1);
-  if (result == node.ku8MBSuccess) {
-    return node.getResponseBuffer(kRegisterVOltage);
+  uint32_t measurement;
+  preTransmission();
+  uint8_t result = node_.readInputRegisters(kRegisterAlarmStatus, 1);
+  if (result == node_.ku8MBSuccess) {
+    measurement = node_.getResponseBuffer(kRegisterAlarmStatus);
   } else {
-    return 0x0001;
+    measurement = 0x0001;
   }
+  postTransmission();
+  return measurement;
 }
 
 void PowerMeter::preTransmission() {
-  digitalWrite(pin_re, HIGH);
-  digitalWrite(pin_de, HIGH);
+  digitalWrite(pin_re_, HIGH);
+  digitalWrite(pin_de_, HIGH);
   delay(1);
 }
 
 void PowerMeter::postTransmission() {
-  digitalWrite(pin_re, LOW);
-  digitalWrite(pin_de, LOW);
+  digitalWrite(pin_re_, LOW);
+  digitalWrite(pin_de_, LOW);
   delay(1);
 }
