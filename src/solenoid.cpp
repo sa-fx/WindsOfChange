e9dc62d0180b4adfd1f622fc0a76623ab8e349b7
Stewart Alexander <66742157+sa-fx@users.noreply.github.com>
2024-02-25 21:27:16 +0000

    Solenoid: Skeleton code

--- src/solenoid.cpp
+++ src/solenoid.cpp
@@ -1,1 +1,22 @@
 #include "solenoid.h"
+
+Solenoid::Solenoid(int pin) : pin_(pin) { plunger_state_ = kRetracted; }
+
+Solenoid::~Solenoid() {
+  // Retract solenoid upon destruction
+  retract();
+}
+
+void Solenoid::extend() {
+  if (plunger_state_ != kExtended) {
+    // TODOLater - implement actuating solenoid
+    plunger_state_ = kExtended;
+  }
+}
+
+void Solenoid::retract() {
+  if (plunger_state_ != kRetracted) {
+    // TODOLater - implement actuating solenoid
+    plunger_state_ = kRetracted;
+  }
+}
--- src/solenoid.h
+++ src/solenoid.h
@@ -1,6 +1,31 @@
 #ifndef __SOLENOID_H__
 #define __SOLENOID_H__
 
-class Solenoid {};
+/**
+ * Possible positions of the solenoid plunger
+ */
+enum SolenoidState { kRetracted = 0, kExtended };
+
+class Solenoid {
+ public:
+  Solenoid(int pin);
+  ~Solenoid();
+
+  /**
+   * @brief Extend the solenoid's plunger
+   * @note If the plunger is already extended, this method will have no effect
+   */
+  void extend();
+
+  /**
+   * @brief Retract the solenoid's plunger
+   * @note If the plunger is already retracted, this method will have no effect
+   */
+  void retract();
+
+ private:
+  int pin_;
+  SolenoidState plunger_state_;
+};
 
 #endif
